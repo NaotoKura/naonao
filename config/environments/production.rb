a5b1cca4fa6f561025e2bdd186bf538b2d0de56f
kura <kura.n@htrust-inc.jp>
2025-12-10 14:29:13 +0900

    デプロイ修正 - URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.hosts << "naonao-production.up.railway.app"
+  config.hosts << ".railway.app"
 end
